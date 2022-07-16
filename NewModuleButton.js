cef303a65b44e9f9eaa1a7325de31589e52af9ce
riku <riku.suny@gmail.com>
2022-07-16 21:05:12 +0900

    add Button

---
+++ NewModuleButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button, NativeModules} from 'react-native';
+
+const {CalendarModule} = NativeModules;
+
+const NewModuleButton = () => {
+  const onPress = () => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+  };
+
+  return (
+    <Button
+      title="Click to invoke your native module!"
+      color="#841584"
+      onPress={onPress}
+    />
+  );
+};
+
+export default NewModuleButton;
