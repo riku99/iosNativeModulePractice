08fe17c8e0c52418b4765e0b3712a098f5e7f4b5
riku <riku.suny@gmail.com>
2022-07-17 16:07:30 +0900

    add swift module

---
+++ ios/Counter.swift
@@ -0,0 +1,8 @@
+//
+//  Counter.swift
+//  iosNativeModulePractice
+//
+//  Created by 黒澤陸 on 2022/07/17.
+//
+
+import Foundation
---
+++ ios/CounterBridge.m
@@ -0,0 +1,8 @@
+//
+//  CounterBridge.m
+//  iosNativeModulePractice
+//
+//  Created by 黒澤陸 on 2022/07/17.
+//
+
+#import <Foundation/Foundation.h>
